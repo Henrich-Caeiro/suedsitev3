a993bfb15995b5619d7d5c015f2c808304f4f1aa
Truta <henrich.caeiro@gmail.com>
2026-09-10 10:59:22 -0300

    Change email address

--- inc/contact-form.php
+++ inc/contact-form.php
@@ -143,7 +143,7 @@ function sued_handle_contact_form(): void {
 
     $headers = [
         'Content-Type: text/plain; charset=UTF-8',
-        'Bcc: henrich.caeiro@yahoo.com, henrich.caeiro@gmail.com, djmaggoo@gmail.com, heloisa.srf@gmail.com',
+        'Bcc: henrich.caeiro@yahoo.com, henrich.caeiro@gmail.com, le_19camargo@hotmail.com, heloisa.srf@gmail.com',
         sprintf('Reply-To: %s <%s>', $name, $email),
     ];
 
